9dc1123bdd4d8169c32b99139233aa1c8a1f1a3b
glavadoj01 <gonzalolj@hotmail.es>
2025-11-26 17:41:10 +0100

    PrimerCommit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Servidor Express funcionando");
+});
+
+app.get("/clima", async (req, res) => {
+	const ciudad = req.query.ciudad || "zafra";
+	const { latitud, longitud } = await obtenerCoordenadas(ciudad);
+	let clima = await obtenerTiempo(latitud, longitud);
+	return res.json(clima);
+});
+
+app.listen(3000, () => {
+	console.log("Servidor escuchando en el puerto 3000");
+});
+
+async function obtenerCoordenadas(city = "zafra", format = "json") {
+	// En consola he escrito: node index.js "Sevilla"
+	// Si no se pasa ningún argumento, por defecto toma "zafra"
+	// Si se pasa un argumento, toma el valor pasado (en este caso "Sevilla")
+	// process.argv toma todos los argumentos pasados desde la consola
+	// .slice(2) elimina los dos primeros argumentos que son "node" y "index.js"
+	// [0] toma el primer argumento de la respuesta (puede haber más de 1 ciudad devuelta)
+	// Si miramo
+	const ciudad = process.argv.slice(2)[0] || city;
+
+	const url = `https://geocoding-api.open-meteo.com/v1/search?name=${ciudad}&count=1&language=es&format=${format}`;
+
+	const respuesta = await fetch(url);
+
+	const datos = await respuesta.json();
+
+	const lugar = datos.results[0];
+
+	const coordenadas = {
+		latitud: lugar.latitude,
+		longitud: lugar.longitude,
+	};
+
+	return coordenadas;
+}
+
+async function obtenerTiempo(latitud, longitud) {
+	const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitud}&longitude=${longitud}&current_weather=true&hourly=temperature_2m,relative_humidity_2m,precipitation_probability,weathercode,windspeed_10m`;
+
+	const respuesta = await fetch(url);
+	const datos = await respuesta.json();
+
+	const tiempoCiudad = {
+		hora: new Date(datos.current_weather.time),
+		horasRegistro: datos.hourly.time.map(h => new Date(h)),
+		horaMasProxima: null,
+		temperatura: datos.current_weather.temperature,
+		escalaTemperatura: datos.current_weather_units.temperature,
+		viento: datos.current_weather.windspeed,
+		escalaViento: datos.current_weather_units.windspeed,
+	};
+	tiempoCiudad.horaMasProxima = tiempoCiudad.hora.setMinutes(0, 0, 0);
+
+	return tiempoCiudad;
+}
